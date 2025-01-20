dc9e5f4e7d911a2ef12ba81cae29780e997562ae
Shahadat1530 <155170141+Shahadat1530@users.noreply.github.com>
2025-01-20 19:48:44 +0600

    setup done

---
+++ index.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+const app = express();
+
+
+app.use(cors());
+app.use(express.json());
+
+
+
+
+
+app.get('/', async (req, res) => {
+    res.send('Dream Scholar Hub');
+});
+
+app.listen(port, () => {
+    console.log(`Server Running on Port: ${port}`);
+})
