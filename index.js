33c1502215828c09e95d825587db719c288e059e
Shahadat1530 <155170141+Shahadat1530@users.noreply.github.com>
2025-01-21 12:27:18 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 const app = express();
 
@@ -10,6 +11,126 @@ app.use(express.json());
 
 
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.c0tb2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+        const userCollection = client.db('scholarDB').collection('users');
+
+
+        // jwt api's
+        // app.post('/jwt', async (req, res) => {
+        //     const user = req.body;
+        //     const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+        //     res.send({ token });
+        // });
+
+
+        // middlewares
+        // const verifyToken = (req, res, next) => {
+        //     // console.log('inside the verify', req.headers.authorization);
+        //     if (!req.headers.authorization) {
+        //         return res.status(401).send({ message: 'unauthorize access' });
+        //     }
+
+        //     const token = req.headers.authorization.split(' ')[1]
+        //     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        //         if (err) {
+        //             return res.status(401).send({ message: 'unauthorize access' });
+        //         }
+        //         req.decoded = decoded;
+        //         next();
+        //     })
+        // };
+
+        // const verifyAdmin = async (req, res, next) => {
+        //     const email = req.decoded.email;
+        //     const query = { email: email };
+        //     const user = await userCollection.findOne(query);
+        //     const isAdmin = user?.role === 'admin';
+        //     if (!isAdmin) {
+        //         return res.status(403).send({ message: 'forbidden access' });
+        //     };
+        //     next();
+        // }
+
+
+
+        // user api's
+        // app.get('/users', verifyToken, verifyAdmin, async (req, res) => {
+        //     const result = await userCollection.find().toArray();
+        //     res.send(result);
+        // });
+
+        app.get('/users/admin/:email', verifyToken, async (req, res) => {
+            const email = req.params.email;
+            if (email !== req.decoded.email) {
+                return res.status(403).send({ message: 'forbidden access' });
+            };
+
+            const query = { email: email };
+            const user = await userCollection.findOne(query);
+            let admin = false;
+            if (user) {
+                admin = user?.role === 'admin';
+            };
+            res.send({ admin });
+
+        });
+
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+
+            const query = { email: user.email };
+            const exitingUser = await userCollection.findOne(query);
+            if (exitingUser) {
+                return res.send({ massage: 'User already exists', insertedId: null })
+            }
+
+            const result = await userCollection.insertOne(user);
+            res.send(result);
+        });
+
+        // app.patch('/users/admin/:id', verifyToken, verifyAdmin, async (req, res) => {
+        //     const id = req.params.id;
+        //     const filter = { _id: new ObjectId(id) };
+        //     const updateDoc = {
+        //         $set: {
+        //             role: 'admin'
+        //         }
+        //     };
+        //     const result = await userCollection.updateOne(filter, updateDoc);
+        //     res.send(result);
+        // });
+
+        // app.delete('/users/:id', verifyToken, verifyAdmin, async (req, res) => {
+        //     const id = req.params.id;
+        //     const query = { _id: new ObjectId(id) };
+        //     const result = await userCollection.deleteOne(query);
+        //     res.send(result);
+        // });
+
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
 
 
 app.get('/', async (req, res) => {
