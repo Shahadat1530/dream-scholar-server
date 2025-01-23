dc43348bf45caf7891bfccae721b9ff64bb37e3d
Shahadat1530 <155170141+Shahadat1530@users.noreply.github.com>
2025-01-23 22:26:52 +0600

    installed jwt, also set token

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 const app = express();
@@ -30,11 +31,11 @@ async function run() {
 
 
         // jwt api's
-        // app.post('/jwt', async (req, res) => {
-        //     const user = req.body;
-        //     const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
-        //     res.send({ token });
-        // });
+        app.post('/jwt', async (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN, { expiresIn: '2h' });
+            res.send({ token });
+        });
 
 
         // middlewares
